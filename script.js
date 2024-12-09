b8b37f322b734bdfccc3732247765159227d8a46
User200925 <dalimovhojiakbar@gmail.com>
2024-12-09 18:40:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function calculate() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let operation = document.getElementById("operation").value;
+
+    let result;
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            result = "Неверное действие!";
+    }
+
+    document.getElementById("result").innerText = `${num1} ${operation} ${num2} =\n${result}`;
+}
